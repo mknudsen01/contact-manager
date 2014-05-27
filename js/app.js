4f6bff39df6646f86f9f60a3de539c349aea7060
Matthew Knudsen <mknudsen01@gmail.com>
2014-05-27 15:19:47 -0700

    Added some styles to the contact view
      * Can toggle more or fewer details
      * See name, phone and email by default
      * Issues:
        * Css is sloppy
        * show and hide functions on contact view sloppy

--- js/app.js
+++ js/app.js
@@ -11,6 +11,34 @@ $(function() {
       title: 'very important person',
       phone: '1234567890'
     },
+
+    {
+      firstName: 'Test',
+      lastName: 'Testerson',
+      email: 'test@email.com',
+      company: 'cool company',
+      title: 'very important person',
+      phone: '1234567890'
+    },
+
+    {
+      firstName: 'Test',
+      lastName: 'Testerson',
+      email: 'test@email.com',
+      company: 'cool company',
+      title: 'very important person',
+      phone: '1234567890'
+    },
+
+    {
+      firstName: 'Test',
+      lastName: 'Testerson',
+      email: 'test@email.com',
+      company: 'cool company',
+      title: 'very important person',
+      phone: '1234567890'
+    },
+
   ];
 
 
