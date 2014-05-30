370a5cd14be4ba6c561b3ebdbe5245828f05760c
Matthew Knudsen <mknudsen01@gmail.com>
2014-05-29 21:18:55 -0700

    Add server.js and configure to port 1414

---
+++ server.js
@@ -0,0 +1,19 @@
+var application_root = __dirname,
+    express = require( 'express' ),
+    path = require( 'path' ),
+    mongoose = require( 'mongoose' );
+
+var app = express();
+
+app.configure( function() {
+  app.use( express.bodyParser() );
+  app.use( express.methodOverride() );
+  app.use( app.router );
+  app.use( express.static( path.join( application_root, 'site' ) ) );
+  app.use( express.errorHandler({ dumpExceptions: true, showStack: true}));
+});
+
+var port = 1414;
+app.listen( port, function() {
+  console.log( 'Express server listening on port %d in %s mode', port, app.settings.env);
+})
