a1b44f993a035505e97cae483ad279b10038ca64
Matthew Knudsen <mknudsen01@gmail.com>
2014-06-03 22:46:44 -0700

    clean up letter Rendering on app initialization
      * alphabet is now a constant
      * renderLetters logic pulled out into its own function

--- site/js/app.js
+++ site/js/app.js
@@ -2,18 +2,21 @@ var app = app || {};
 
 var TO_CHECK = ["firstName", "lastName", "phone", "email", "title", "company", "city"];
 var ENTER_KEY = 13;
+var ALPHABET = ['all','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 $(function() {
-  var alphabet = ['all','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-  for(var i=0; i< alphabet.length; i++){
+  renderLetters();
+  new app.RolodexView();
+});
+
+var renderLetters = function(){
+  for(var i=0; i< ALPHABET.length; i++){
     var tab = document.createElement('article');
     tab.dataset.name="letter-tab";
     tab.className ="letter padding-tb-1 info tab disp-ib w-small";
     if(i === 0){
       tab.className += " all-letters highlight";
     }
-    tab.innerHTML = alphabet[i].toUpperCase();
+    tab.innerHTML = ALPHABET[i].toUpperCase();
     $('.letters').append(tab);
   }
-
-  new app.RolodexView();
-});
+};
